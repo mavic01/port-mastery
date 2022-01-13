9095074b488a4b11409d780742eb2d86fa708c16
Victor Iberi <iberivictor7@gmail.com>
2022-01-13 17:14:11 +0100

    another commit

--- src/data.js
+++ src/data.js
@@ -9,28 +9,28 @@ export const webData = [
     {
         id: 2,
         title: "Task Tracker App",
-        desc: "This app was built using ReactJS and deployed using netlify.",
+        desc: "This app was built using React.JS and deployed using netlify.",
         img: "assets/task-tracker-app.png",
         url: "https://mavic-task-tracker.netlify.app/"
     },
     {
         id: 3,
-        title: "Google Clone",
-        desc: "This app was built using ReactJS and tailwind css. Data was fetched from rapid API using axios.",
-        img: "assets/search.jpg",
-        url: "https://mavic-search.netlify.app/search"
+        title: "Mavic Art Website",
+        desc: "This app was built using Next.JS.",
+        img: "assets/artt.jpg",
+        url: "http://mavicart.vercel.app/"
     },
     {
         id: 4,
         title: "Chat App",
-        desc: "This app was built using NextJS and deployed with vercel.",
+        desc: "This app was built using Next.JS and deployed with vercel.",
         img: "assets/chat.jpg",
         url: "https://nextjs-chat-three.vercel.app/"
     },
     {
         id: 5,
         title: "React CRUD App",
-        desc: "This app was built using ReactJS and context API.",
+        desc: "This app was built using React.JS and context API.",
         img: "assets/react-crud-app.png",
         url: "https://mavic-my-people-app.netlify.app/"
     },
@@ -44,7 +44,7 @@ export const webData = [
     {
         id: 7,
         title: "Movie Application",
-        desc: "This app was built using ReactJS, Movie Database API and styled-components",
+        desc: "This app was built using React.JS, Movie Database API and styled-components",
         img: "assets/movies.jpg",
         url: "https://mavic-movies.netlify.app/"
     },
@@ -59,8 +59,17 @@ export const webData = [
         id: 9,
         title: "Weather App",
         desc: "This app was built using HTML, CSS and Node in the backend.",
-        img: "assets/weather-app.png",
+        img: "assets/weather-app.jpg",
         url: "https://mavic-wx.herokuapp.com/"
+    },
+    {
+        id: 10,
+        title: "Google Clone",
+        desc: "This app was built using React.JS and tailwind css. Data was fetched from rapid API using axios.",
+        img: "assets/search.jpg",
+        url: "https://mavic-search.netlify.app/search"
     }
 ]
+
+
 
